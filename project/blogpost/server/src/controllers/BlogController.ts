7dc5b17fdd8560613dfecc8becfe6a7f35266147
Fezao187 <katlegomaredi9@gmail.com>
2024-07-28 13:32:53 +0200

    Create new blog controller and route

---
+++ project/blogpost/server/src/controllers/BlogController.ts
@@ -0,0 +1,42 @@
+import express from "express";
+import mongoose from "mongoose";
+import { Blogpost } from "../models/Blogpost.js";
+
+// Create blogpost
+export const CreateBlog = async (
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+) => {
+    try {
+        // Check if all fields are sent
+        if (!req.body.title || !req.body.body) {
+            return res.status(400)
+                .send({
+                    message: "Send all required fields"
+                });
+        }
+
+        // Create blog interface
+        interface BlogInterface {
+            title: string,
+            body: string,
+            username?: string,
+            creator?: mongoose.Types.ObjectId
+        }
+
+        // Create blog interface
+        const blogObj: BlogInterface = {
+            title: req.body.title,
+            body: req.body.body
+        }
+
+        // Save blogposts to DB
+        const blog = await Blogpost.create(blogObj);
+        res.status(201)
+            .json({ message: "Blog post created successfully" });
+        next();
+    } catch (error) {
+        console.log(error.message);
+    }
+}
--- project/blogpost/server/src/routes/AppRoutes.ts
+++ project/blogpost/server/src/routes/AppRoutes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { Login, Signup } from "../controllers/AuthController.js";
+import { CreateBlog } from "../controllers/BlogController.js";
 
 const router = Router();
 router.post("/signup", Signup);
 router.post("/login", Login);
+router.post("/create/blog", CreateBlog);
 
 export default router;
