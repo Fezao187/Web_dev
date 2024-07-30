9738072b2f96f9d496315605961e3ca637b4b14c
Fezao187 <katlegomaredi9@gmail.com>
2024-07-30 20:11:39 +0200

    Update and delet blog functions

--- project/blogpost/server/src/controllers/BlogController.ts
+++ project/blogpost/server/src/controllers/BlogController.ts
@@ -73,14 +73,77 @@ export const getAllBlogs = async (
 ) => {
     try {
         // Get all blogposts
-        const blogs=await Blogpost.find({});
+        const blogs = await Blogpost.find({});
         res.status(201)
-        .json({
-            message:"All blogs retrived",
-            data: blogs
-        });
+            .json({
+                message: "All blogs retrived",
+                data: blogs
+            });
         next();
     } catch (error) {
         console.log(error.message);
+    }
+}
+
+// Edit blog
+export const editBlog = async (
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+) => {
+    // Check if all fields are filled in
+    try {
+        if (!req.body.title || !req.body.body) {
+            return res.status(400)
+                .send({
+                    message: "Send all required fields"
+                });
+        }
+
+        // Get id from params
+        const { id } = req.params;
+        // Update blog
+        const result = await Blogpost.findByIdAndUpdate(id, {
+            title: req.body.title,
+            body: req.body.body
+        });
+
+        if (!result) {
+            return res.status(404)
+                .json({
+                    message: "Blogpost not found"
+                });
+        }
+        next();
+        return res.status(200)
+            .json({ message: "Blogpost updated successfully" });
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send({ message: error.message })
+    }
+}
+
+// Delete blog
+export const deleteBlogpost = async (
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+) => {
+    try {
+        // get id from params
+        const { id } = req.params;
+        // Use ID to delete blogpost
+        const result = await Blogpost.findByIdAndDelete(id);
+
+        if (!result) {
+            return res.status(404)
+                .json({ message: "Blogpost not found" });
+        }
+        next();
+        return res.status(200)
+            .send({ message: "Blog deleted successfully" });
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send({ message: error.message });
     }
 }
--- project/blogpost/server/src/routes/AppRoutes.ts
+++ project/blogpost/server/src/routes/AppRoutes.ts
@@ -1,13 +1,15 @@
 import { Router } from "express";
 import { Login, Signup } from "../controllers/AuthController.js";
-import { CreateBlog, getAllBlogs, getBlogpostById } from "../controllers/BlogController.js";
+import { CreateBlog, deleteBlogpost, editBlog, getAllBlogs, getBlogpostById } from "../controllers/BlogController.js";
 import { userVerification } from "../middleware/AuthMiddleware.js";
 
 const router = Router();
 router.post("/signup", Signup);
 router.post("/login", Login);
 router.post("/create/blog", userVerification, CreateBlog);
-router.get("/:id",getBlogpostById);
-router.get("/",getAllBlogs);
+router.get("/:id", getBlogpostById);
+router.get("/", getAllBlogs);
+router.put("/:id", editBlog);
+router.delete("/:id", deleteBlogpost);
 
 export default router;
