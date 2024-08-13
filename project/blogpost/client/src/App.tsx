431e8dd2e1365d8c36b513a9b95a58ca5ffc97a8
Fezao187 <katlegomaredi9@gmail.com>
2024-08-13 22:41:43 +0200

    Initialise react app with tailwind and Typescript

---
+++ project/blogpost/client/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <h1 className='text-3xl font-bold underline text-red-600'>
+      Simple React TypeScript Tailwind Simple.
+    </h1>
+  );
+}
+
+export default App;
