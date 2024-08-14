cba5a62691e0ffe52a89eadb6187c38012f1876e
Fezao187 <katlegomaredi9@gmail.com>
2024-08-14 19:36:47 +0200

    Blog container component

---
+++ project/blogpost/client/src/components/BlogsContainer.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+type Props = {}
+
+const BlogsContainer = (props: Props) => {
+  return (
+    <div>
+        <div><span>Profile Pic</span><span>Name</span></div>
+        <div>Title</div>
+        <div>Blogs</div>
+    </div>
+  )
+}
+
+export default BlogsContainer;
--- project/blogpost/client/src/pages/Home.tsx
+++ project/blogpost/client/src/pages/Home.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import BlogsContainer from '../components/BlogsContainer'
 
 type Props = {}
 
 const Home = (props: Props) => {
     return (
-        <div>Home</div>
+        <>
+            <BlogsContainer />
+        </>
     )
 }
 
